9c586a86f3da8d52e5b92e3da9537b7381b3b936
munkhtaivan <munkhtaiwan000@gmail.com>
2024-06-21 15:05:48 +0900

    できたやつ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Defines the roof path route ("/")
+  # root "articles#index"
+  # localhost:3000/parts
+  resources :parts, only: [:index]
+
 end
